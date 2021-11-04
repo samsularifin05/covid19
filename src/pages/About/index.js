3aeeb20fdc20c04e53dcfcd60cb1e8ccfffa91f2
Samsul Arifin <samsul026@gmail.com>
2021-11-04 21:21:26 +0700

    first commit

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet, Text, View} from 'react-native';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
 import {MyProfil} from '../../assets';
 import Gap from '../../components/Gap';
 import {colors} from '../../utils';
@@ -9,15 +9,33 @@ const Static = () => {
     <View style={styles.pages}>
       <View>
         <Gap height={20} />
-        <Gap height={20} />
+      </View>
+      <View style={styles.content}>
         <View style={styles.contentDayTotal}>
           <Image source={MyProfil} style={styles.images} />
         </View>
         <View style={styles.myname}>
           <Text style={styles.judul}> Samsul Arifin </Text>
+          <Gap height={20} />
+          <Button
+            title="Git : samsularifin05"
+            color="#000000"
+            style={styles.button}
+          />
+          <Gap height={20} />
+          <Button
+            style={styles.button}
+            title="FB : samsularifin30"
+            color="#1095F4"
+          />
+          <Gap height={20} />
+          <Button
+            title="LinkIn : samsularifin05"
+            style={styles.button}
+            color="#7FB3FF"
+          />
         </View>
       </View>
-      <View style={styles.content}></View>
     </View>
   );
 };
@@ -25,15 +43,16 @@ const Static = () => {
 export default Static;
 
 const styles = StyleSheet.create({
+  button: {
+    borderRadius: 60,
+  },
   myname: {
     alignItems: 'center',
     justifyContent: 'center',
   },
   judul: {
-    marginLeft: 12,
     marginTop: 12,
-    fontSize : 28,
-    color: colors.white,
+    fontSize: 28,
   },
 
   contentDayTotal: {
@@ -42,6 +61,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   images: {
+    marginTop: -80,
     width: 150,
     height: 150,
     borderRadius: 70,
@@ -56,7 +76,7 @@ const styles = StyleSheet.create({
     borderRadius: 40,
     borderBottomLeftRadius: 0,
     borderBottomRightRadius: 0,
-    marginTop: '10%',
+    marginTop: '90%',
     paddingTop: 1,
   },
 });
